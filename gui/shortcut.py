c12ea8740e7acdc891c8e53ae36503da97de4dc6
ianpengg <ian01050.ee10@nycu.edu.tw>
2023-04-17 22:55:45 +0800

    🎨 Refactor: seperate the shortcut from controller

---
+++ gui/shortcut.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QShortcut
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtCore import Qt
+
+
+class Shortcut:
+    """
+    Use this class to control the shortcut of the program
+    You can easily add new shortcut by adding new function in the Buttoncontroller class
+    and add the shortcut in the __init__ function
+    """
+    def __init__(self, MainWidget,  Buttoncontroller):
+        self.Buttoncontroller = Buttoncontroller
+        self.MainWidget = MainWidget
+
+        # Use to control go prev
+        QShortcut(QKeySequence(Qt.Key_A), self.MainWidget).activated.connect(self.Buttoncontroller.on_prev)
+        
+        # Use to control go next
+        QShortcut(QKeySequence(Qt.Key_D), self.MainWidget).activated.connect(self.Buttoncontroller.on_next)
+
+        # Use to control play and pause
+        QShortcut(QKeySequence('p'), self.MainWidget).activated.connect(self.Buttoncontroller.on_play)
+
+        # Use to reduce brush_size
+        QShortcut(QKeySequence(Qt.Key_1), self.MainWidget).activated.connect(self.Buttoncontroller.on_brsize_minus)
+
+        # Use to increase brush_size
+        QShortcut(QKeySequence(Qt.Key_2), self.MainWidget).activated.connect(self.Buttoncontroller.on_brsize_plus)
+
+        # Use to control reset
+        QShortcut(QKeySequence(Qt.Key_R), self.MainWidget).activated.connect(self.Buttoncontroller.on_reset)
+
+        # Use to control zoom
+        QShortcut(QKeySequence(Qt.Key_E), self.MainWidget).activated.connect(self.Buttoncontroller.on_erase)
+
+        # Use to control save
+        QShortcut(QKeySequence('Ctrl+S'), self.MainWidget).activated.connect(self.Buttoncontroller.on_save)
+
+        # Use to control undo
+        QShortcut(QKeySequence('Ctrl+Z'), self.MainWidget).activated.connect(self.Buttoncontroller.on_undo)
+
+        # Use to control infer
+        QShortcut(QKeySequence(Qt.Key_I), self.MainWidget).activated.connect(self.Buttoncontroller.on_infer)   
