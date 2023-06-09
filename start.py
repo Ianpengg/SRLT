136bf36550c68920a0670bb64c053e336c4c003d
ianpengg <ian01050.ee10@nycu.edu.tw>
2023-06-09 13:33:44 +0800

    ✨ feat: add the support for radiate dataset

--- start.py
+++ start.py
@@ -1,39 +1,44 @@
-
-import os 
-#import torch
+import os
+import sys
 import numpy as np
 import argparse
 
 from PyQt5 import QtWidgets
 
 from gui.controller import MainWindow_controller
-from segment_anything import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
+from segment_anything import sam_model_registry, SamPredictor
+from utils.timestamp_loader import registry
 
 
+if __name__ == "__main__":
 
-def load_timestamp(root, log_name):
-
-    radar_folder = root + 'oxford/' + log_name + '/'
-    timestamps_path = radar_folder + 'radar.timestamps'
-    radar_timestamps = np.loadtxt(timestamps_path, delimiter=' ', usecols=[0], dtype=np.int64)
-
-    return radar_timestamps
-
-
-if __name__ == '__main__':
-    import sys
-     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--data_root", type=str, help="specify the datapath")
-    parser.add_argument("--checkpoint", type=str, default="./models/sam_vit_h_4b8939.pth", help="specify the checkpoint path")
-    parser.add_argument("--model_type", type=str, default="vit_h", help="specify the model type")
+    parser.add_argument(
+        "-f", "--folder_name", type=str, help="specify the folder name to labeled"
+    )
+    parser.add_argument(
+        "-d", "--data_root", type=str, help="specify the path where the data locate"
+    )
+    parser.add_argument(
+        "--dataset", type=str, default="oxford", help="specify which dataset is used"
+    )
+
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default="./models/sam_vit_h_4b8939.pth",
+        help="specify the checkpoint path",
+    )
+    parser.add_argument(
+        "--model_type", type=str, default="vit_h", help="specify the model type"
+    )
     args = parser.parse_args()
 
+    data_root = os.path.join(args.data_root, args.folder_name)
 
-    root = '/media/ee904/data/'
-    log_name = '2019-01-10-11-46-21-radar-oxford-10k' 
-    data_root = args.data_root
-    radar_timestamps = load_timestamp(root, log_name)
+    timestamp_loader = f"{args.dataset}_timestamp_loader"
+
+    radar_timestamps = registry.get(timestamp_loader)(args.data_root)
 
     sam_checkpoint = args.checkpoint
     model_type = args.model_type
@@ -42,9 +47,7 @@ if __name__ == '__main__':
 
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     sam.to(device=device)
-
     sam_controller = SamPredictor(sam)
-    
 
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow_controller(data_root, radar_timestamps, sam_controller)
---
+++ utils/timestamp_loader.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from .registry import Registry
+
+registry = Registry("timestamp_loader")
+
+
+@registry.register()
+def oxford_timestamp_loader(root_path):
+    radar_folder = root_path
+    timestamps_path = os.path.join(radar_folder, "radar.timestamps")
+    radar_timestamps = np.loadtxt(
+        timestamps_path, delimiter=" ", usecols=[0], dtype=np.int64
+    )
+    return radar_timestamps
+
+
+@registry.register()
+def radiate_timestamp_loader(root_path):
+    radar_folder = root_path
+    timestamps_path = os.path.join(radar_folder, "Navtech_Cartesian.txt")
+    radar_timestamps = np.loadtxt(
+        timestamps_path, delimiter=" ", usecols=[3], dtype=np.float64
+    )
+    return radar_timestamps
